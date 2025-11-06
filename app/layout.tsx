333f03432484ef18a91dede7d3e679fb669421cc
Tran Tien Dat (K.NHS-HO) <dattt4@pvcombank.com.vn>
2025-11-06 15:37:31 +0700

    feat: init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,19 @@ export const metadata: Metadata = {
   description: "Giải pháp CMMN",
   keywords: ["CMMN", "POC", "CMMN", "CMMN", "CMMN"],
   openGraph: {
+    type: "website",
     title: "POC CMMN",
     description: "Giải pháp CMMN",
     url: "https://poc-cmmn.vercel.app",
     siteName: "POC CMMN",
+    locale: "vi_VN",
     images: [
       {
         url: "https://www.syte.ai/wp-content/uploads/2021/10/Glossary-Images-1-24_23-1024x625.jpg",
         width: 1024,
-        height: 625
+        height: 625,
+        alt: "POC CMMN",
+        type: "image/jpeg"
       }
     ]
   },
@@ -35,7 +39,10 @@ export const metadata: Metadata = {
     title: "POC CMMN",
     description: "Giải pháp CMMN",
     images: [
-      "https://www.syte.ai/wp-content/uploads/2021/10/Glossary-Images-1-24_23-1024x625.jpg"
+      {
+        url: "https://www.syte.ai/wp-content/uploads/2021/10/Glossary-Images-1-24_23-1024x625.jpg",
+        alt: "POC CMMN"
+      }
     ]
   },
   robots: {
